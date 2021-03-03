d3ae49194ca0327214aa181c97e8f68577f627bb
Aamir Mahmood <mohdaamir8182@gmail.com>
2021-03-03 16:23:26 +0500

    refactor:(design optimizations)

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,39 +1,42 @@
-import React from 'react'
-import { StyleSheet, Text, TouchableOpacity, Image } from 'react-native'
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity, Image} from 'react-native';
 
 const Button = (props) => {
+  const {title, onPress} = props;
 
-    const { title , onPress } = props;
-
-    return (
-        <TouchableOpacity 
-            style={styles.container} 
-            onPress={onPress}
-        >
-            <Image source={require("../assets/fb.png")} style={{height: 45, width: 45, tintColor: '#fff'}} resizeMode="contain" />
-            <Text style={styles.title}>{title}</Text>
-        </TouchableOpacity>
-    )
-}
+  return (
+    <TouchableOpacity style={styles.container} onPress={onPress}>
+      <Image
+        source={require('../assets/fb.png')}
+        style={styles.fbImage}
+        resizeMode="contain"
+      />
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
-    container:{
-        flexDirection: 'row',
-        height: 60,
-        paddingLeft: 20,
-        alignItems: 'center',
-        justifyContent: "space-between",
-        backgroundColor: 'blue',
-        borderRadius: 10
-    },
-    title:{
-        color: "#fff",
-        fontWeight: 'bold',
-        fontSize: 14,
-        marginHorizontal: 30
-    }
-})
+  container: {
+    flexDirection: 'row',
+    height: 60,
+    paddingLeft: 20,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: 'blue',
+    borderRadius: 10,
+  },
+  title: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 14,
+    marginHorizontal: 30,
+  },
+  fbImage: {
+    height: 40,
+    width: 40,
+    tintColor: '#fff',
+  },
+});
 
-export default Button
-
-
+export default Button;
